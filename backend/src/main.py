ab473659da16bb97262d3c0582456561d5fee29a
jeffreyw9998 <jeffreyw9998@gmail.com>
2023-04-09 21:00:49 -0700

    updated main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,7 +2,7 @@ from typing import List
 
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import session
-from . import models
+from . import models, crud, schemas
 from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
